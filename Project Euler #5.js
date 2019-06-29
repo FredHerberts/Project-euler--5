47c22bd79bafbfb4761940a81c4463373d344350
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-29 07:48:35 +0200

    Update Project Euler #5.js

--- Project Euler #5.js
+++ Project Euler #5.js
@@ -1,1 +1,1 @@
-var primelist = [2,3,5,7,11,13,17,19] ; var LCM = 1;for (x=0; x<9;x++){var y = 1;while(y<20){y=y*primelist[x];if (20>y){LCM *= primelist[x]}}};alert(LCM)
+var primelist = [2,3,5,7,11,13,17,19] ; var x = 0; var LCM = 1;for (x=0; x<9;x++){var y = 1;while(y<20){y=y*primelist[x];if (20>y){LCM *= primelist[x]}}};alert(LCM)
