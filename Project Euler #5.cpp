96ad7443398af51587b2dea4ea0ad9f0f9695e9f
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-05 22:59:12 +0200

    Create Project Euler #5.cpp

---
+++ Project Euler #5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <array>
+using namespace std;
+int main()
+{
+	int primelist[] = { 2,3,5,7,11,13,17,19 };
+	int primes = sizeof(primelist) / 4;
+	int LCM = 1;
+	for (int x = 0; x < sizeof(primelist) / 4; x++) {
+		int z = primelist[x];
+		int y = 1;
+		while (20 > y) {
+			y = y * z;
+			if (20 > y) {
+				LCM = LCM * z;
+			}
+		}
+	}
+	cout << LCM << endl;
+	return 0;
+}
