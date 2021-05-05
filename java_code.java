9abe512df73abfc3b4ca57937db9a67d8ad32f1c
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2021-05-05 19:25:53 +0200

    Create java_code.java

---
+++ java_code.java
@@ -0,0 +1,19 @@
+package com.codewithmost;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int LCM = 1;
+        int z = 1;
+        List<Integer> primelist = Arrays.asList(2,3,5,7,11,13,17,19);
+        for (int x = 1; x < 9; x++) {
+            int y = 1;
+            while (20 > y*primelist.get(x-1)){
+                y = y*primelist.get(x-1);
+                LCM = LCM*primelist.get(x-1);
+            }
+        }
+        System.out.println(LCM);
+    }
+}
